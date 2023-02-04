6dcd879adb674b4861688a7b59aad8bbe149f7da
Marcos Barbosa <mvpbarbosa16@gmail.com>
2023-02-03 21:42:19 -0300

    feat(Chart): implement Chart

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,63 +1,134 @@
+/* eslint-disable react/jsx-no-bind */
+import { useEffect, useState } from 'react'
 import {
-  Block,
+  Square,
   ChartContainer,
   ChartDiv,
-  ChartItem,
   Dash,
-  Legend,
+  LegendItem,
   LegendDiv,
   Percentage,
   SelectTimeButton,
   Time,
   Title
 } from './styled'
-import React from 'react'
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend
+} from 'chart.js'
+
+import { Bar } from 'react-chartjs-2'
+
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend)
 
 export default function Chart() {
+  interface IData {
+    name: string
+    percent: number
+    color: string
+  }
+  const Data: IData[] = [
+    {
+      name: 'Sono',
+      percent: 5,
+      color: 'red'
+    },
+    {
+      name: 'Sono',
+      percent: 10,
+      color: 'orange'
+    },
+    {
+      name: 'Sono',
+      percent: 15,
+      color: 'yellow'
+    },
+    {
+      name: 'Sono',
+      percent: 20,
+      color: 'green'
+    },
+    {
+      name: 'Sono',
+      percent: 25,
+      color: 'blue'
+    },
+    {
+      name: 'Sono',
+      percent: 25,
+      color: 'indigo'
+    }
+  ]
+
+  const ChartData = {
+    labels: ['', '', '', '', '', ''],
+    datasets: [
+      {
+        label: ['Sono'],
+        data: [5, 10, 15, 20, 25, 25],
+        backgroundColor: ['red', 'orange', 'yellow', 'green', 'blue', 'indigo'],
+        borderColor: 'black',
+        borderWidth: 1
+      }
+    ]
+  }
+
+  const options = {}
+
+  const [toggle, setToggle] = useState(true)
+  const [btnColor, setBtnColor] = useState('#ffffff')
+
+  useEffect(() => {
+    setBtnColor(state => (toggle ? '#ffffff' : '#aea0f8'))
+  }, [toggle])
+
   return (
     <ChartContainer>
       <Title>ESTATÍSTICAS DA SEMANA</Title>
       <ChartDiv>
         <LegendDiv>
-          <Legend>
-            <Block />
-            <Percentage>Sono 5%</Percentage>
-          </Legend>
-          <Legend>
-            <Block />
-            <Percentage>Sono 5%</Percentage>
-          </Legend>
-          <Legend>
-            <Block />
-            <Percentage>Sono 5%</Percentage>
-          </Legend>
-          <Legend>
-            <Block />
-            <Percentage>Sono 5%</Percentage>
-          </Legend>
-          <Legend>
-            <Block />
-            <Percentage>Sono 5%</Percentage>
-          </Legend>
-          <Legend>
-            <Block />
-            <Percentage>Sono 5%</Percentage>
-          </Legend>
+          {Data.map((el, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <LegendItem key={index}>
+              <Square color={el.color} />
+              <Percentage>
+                {el.name} {el.percent}%
+              </Percentage>
+            </LegendItem>
+          ))}
         </LegendDiv>
-        <ChartItem />
+
+        <Bar data={ChartData} options={options} />
+
         <Time>
           <Title>GRÁFICO</Title>
-          <SelectTimeButton>
+          <SelectTimeButton
+            color={btnColor}
+            onClick={e => {
+              setToggle(state => !state)
+            }}>
             <Dash />
-            <span>Semana</span>
+            <span>Barra</span>
           </SelectTimeButton>
-          <SelectTimeButton>
+          <SelectTimeButton
+            color={btnColor}
+            onClick={e => {
+              setToggle(state => !state)
+            }}>
             <Dash />
-            <span>Mês</span>
+            <span>Pizza</span>
           </SelectTimeButton>
-          <SelectTimeButton>
+          <SelectTimeButton
+            color={btnColor}
+            onClick={e => {
+              setToggle(state => !state)
+            }}>
             <Dash />
-            <span>Ano</span>
+            <span>Polar</span>
           </SelectTimeButton>
         </Time>
       </ChartDiv>
--- src/components/Chart/styled.ts
+++ src/components/Chart/styled.ts
@@ -5,12 +5,12 @@ export const ChartContainer = styled.div`
   flex-direction: column;
   width: 68%;
   height: 50%;
-  padding: 1.938rem 1.25rem 1.938rem 1.25rem;
+  padding: 1.9rem 1.25rem 1.9rem 1.25rem;
   border-radius: 1.875rem;
   background-color: white;
+  font-family: Arial, Helvetica, sans-serif;
 `
 export const Title = styled.span`
-  font-family: Arial, Helvetica, sans-serif;
   font-weight: bold;
   font-size: 2rem;
   color: #787878;
@@ -26,28 +26,24 @@ export const ChartDiv = styled.div`
 export const LegendDiv = styled.div`
   display: flex;
   flex-direction: column;
+  height: 100%;
 `
-export const Legend = styled.div`
+export const LegendItem = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: start;
   align-items: center;
   padding: 0.5rem;
 `
-export const Block = styled.div`
-  width: 1.63rem;
-  height: 1.63rem;
-  background-color: red;
+export const Square = styled.div`
+  width: 1.6rem;
+  height: 1.6rem;
+  background-color: ${props => props.color};
   margin-right: 0.8rem;
 `
 export const Percentage = styled.span`
   font-size: 0.875rem;
   font-family: Arial, Helvetica, sans-serif;
   font-weight: bold;
-`
-export const ChartItem = styled.div`
-  width: 40rem;
-  height: 17rem;
-  background-color: lightgray;
 `
 export const Time = styled.div`
   display: flex;
@@ -60,7 +56,7 @@ export const SelectTimeButton = styled.button`
   padding: 0.5rem;
   border-radius: 0.8rem;
   border: solid 3px #aea0f8;
-  background-color: #ffffff;
+  background-color: ${props => props.color};
   align-items: center;
   color: #787878;
   font-size: 1.5rem;
@@ -71,12 +67,15 @@ export const SelectTimeButton = styled.button`
   &:hover {
     background-color: #aea0f8;
     color: #ffffff;
+    & div {
+      background-color: '';
+    }
   }
 `
 export const Dash = styled.div`
-  width: 0.464rem;
-  height: 1.624rem;
-  border-radius: 25%;
+  width: 0.5rem;
+  height: 1.6rem;
+  border-radius: 1rem;
   background-color: #aea0f8;
   margin-right: 0.5rem;
   transition: all ease-in-out 0.2s;
