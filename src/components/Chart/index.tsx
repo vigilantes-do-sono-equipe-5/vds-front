55536856b9b8b34e19935d9be526cf43b87627f1
Marcos Barbosa <mvpbarbosa16@gmail.com>
2023-02-10 20:40:16 -0300

    refactor(Chart): componentizing chart component

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,13 +1,10 @@
 // import { v4 as uuidv4 } from 'uuid'
+import { Menu } from './Menu'
 import { useEffect, useState } from 'react'
 import {
-  Square,
   ChartContainer,
   ChartDiv,
   Dash,
-  LegendItem,
-  LegendDiv,
-  Percentage,
   SelectTimeButton,
   Time,
   Title
@@ -26,78 +23,31 @@ import { Bar } from 'react-chartjs-2'
 ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend)
 
 export default function Chart(): JSX.Element {
-  interface IData {
-    id: string
-    name: string
-    percent: number
-    color: string
-  }
-  const LegendData: IData[] = [
-    {
-      id: 'abcd1',
-      name: 'Sono',
-      percent: 5,
-      color: 'red'
-    },
-    {
-      id: 'abcd2',
-      name: 'Aaaa',
-      percent: 10,
-      color: 'orange'
-    },
-    {
-      id: 'abcd3',
-      name: 'Bbbb',
-      percent: 15,
-      color: 'yellow'
-    },
-    {
-      id: 'abcd4',
-      name: 'Cccc',
-      percent: 20,
-      color: 'green'
-    },
-    {
-      id: 'abcd5',
-      name: 'Dddd',
-      percent: 25,
-      color: 'blue'
-    },
-    {
-      id: 'abcd6',
-      name: 'Eeee',
-      percent: 25,
-      color: 'indigo'
-    }
-  ]
-
   // Função para enviar os datos formatados para o gráfico do jeito que ele pede com as labels e os datasets
-  const formatData = (data: IData[]) => {
-    const labels = ['', '', '', '', '', '']
-    const datasets = [
+  const formatData = {
+    labels: ['', '', '', '', '', ''],
+    datasets: [
       {
-        barThickness: 50,
+        barThickness: 55,
         label: 'My Data',
-        data: data.map(item => item.percent),
-        backgroundColor: data.map(item => item.color),
+        data: ['5', '10', '15', '20', '25', '25'],
+        backgroundColor: [
+          'red',
+          'yellow ',
+          'orange',
+          'green',
+          'blue',
+          'indigo'
+        ],
         borderColor: 'black',
         borderWidth: 1
       }
     ]
-    return { labels, datasets }
   }
 
   const [toggle, setToggle] = useState(true)
   const [btnColor, setBtnColor] = useState('#ffffff')
   const [selectedItems, setSelectedItems] = useState<string[]>([])
-
-  const handleCheckboxChange = (id: string) => {
-    if (selectedItems.includes(id)) {
-      setSelectedItems(selectedItems.filter(i => i !== id))
-    } else {
-      setSelectedItems([...selectedItems, id])
-    }
-  }
 
   useEffect(() => {
     setBtnColor(state => (toggle ? '#ffffff' : '#aea0f8'))
@@ -107,31 +57,13 @@ export default function Chart(): JSX.Element {
     <ChartContainer>
       <Title>ESTATÍSTICAS DA SEMANA</Title>
       <ChartDiv>
-        <LegendDiv>
-          {LegendData.map(el => (
-            <LegendItem key={el.id}>
-              <input
-                checked={selectedItems.includes(el.id)}
-                onChange={() => handleCheckboxChange(el.id)}
-                type='checkbox'
-              />
-
-              <Square color={el.color} />
-              <Percentage>
-                {el.name} {el.percent}%
-              </Percentage>
-            </LegendItem>
-          ))}
-        </LegendDiv>
-        F
+        <Menu />
         <Bar
-          data={formatData(
-            LegendData.filter(item => selectedItems.includes(item.id))
-          )}
+          data={formatData}
           options={{
             aspectRatio: 2,
             responsive: true,
-            layout: { padding: { right: 10 } },
+            layout: { padding: { left: 30, right: 30, top: 40, bottom: 20 } },
             plugins: {
               title: {
                 display: true
@@ -143,30 +75,18 @@ export default function Chart(): JSX.Element {
           }}
         />
         <Time>
-          <Title>GRÁFICO</Title>
-          <SelectTimeButton
-            color={btnColor}
-            onClick={e => {
-              setToggle(state => !state)
-            }}>
+          <h2>GRÁFICO</h2>
+          <SelectTimeButton>
             <Dash />
-            <span>Barra</span>
+            <h3>Barra</h3>
           </SelectTimeButton>
-          <SelectTimeButton
-            color={btnColor}
-            onClick={e => {
-              setToggle(state => !state)
-            }}>
+          <SelectTimeButton>
             <Dash />
-            <span>Pizza</span>
+            <h3>Pizza</h3>
           </SelectTimeButton>
-          <SelectTimeButton
-            color={btnColor}
-            onClick={e => {
-              setToggle(state => !state)
-            }}>
+          <SelectTimeButton>
             <Dash />
-            <span>Polar</span>
+            <h3>Polar</h3>
           </SelectTimeButton>
         </Time>
       </ChartDiv>
--- src/components/Chart/styled.ts
+++ src/components/Chart/styled.ts
@@ -1,88 +1,85 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const ChartContainer = styled.div`
+  max-width: 55rem;
+  min-width: 30rem;
+  width: 100%;
+  max-height: 23rem;
+  min-height: 10rem;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  max-width: 50rem;
-  max-height: 25rem;
-  padding: 1.9rem 1.25rem 1.9rem 1.25rem;
+  justify-content: space-around;
+  align-items: center;
   border-radius: 1.875rem;
   background-color: white;
-  font-family: Arial, Helvetica, sans-serif;
+  & h2 {
+    font-size: 1.5rem;
+    color: #787878;
+  }
+  & h3 {
+    font-size: 1.3rem;
+  }
 `
-export const Title = styled.span`
+export const Title = styled.h1`
   font-weight: bold;
   font-size: 2rem;
   color: #787878;
 `
 export const ChartDiv = styled.div`
-  width: 70%;
-  max-height: 100%;
+  max-width: 40rem;
+  min-width: 10rem;
+  width: 100%;
+  max-height: 20rem;
+  min-height: 10rem;
+  height: 100%;
   display: flex;
-  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Time = styled.div`
+  max-width: 10rem;
+  min-width: 5rem;
+  width: 100%;
+  max-height: 10rem;
+  min-height: 4rem;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  padding: 1.25rem;
-`
-export const LegendDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  max-height: 100%;
-  max-width: 50%;
-  color: black;
-`
-export const LegendItem = styled.div`
-  display: flex;
-  justify-content: start;
-  align-items: center;
-  padding: 0.2rem;
-  margin-right: 0.5rem;
-  max-height: 80%;
-`
-export const Square = styled.div`
-  min-width: 1.6rem;
-  min-height: 1.6rem;
-  background-color: ${props => props.color};
-  margin-right: 0.8rem;
-`
-export const Percentage = styled.span`
-  font-size: 0.875rem;
-  font-family: Arial, Helvetica, sans-serif;
-  font-weight: bold;
-`
-export const Time = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  margin-left: 0.5rem;
 `
 export const SelectTimeButton = styled.button`
+  max-width: 5rem;
+  min-width: 2rem;
+  width: 100%;
+  max-height: 2.2rem;
+  min-height: 1.2rem;
+  height: 100%;
   display: flex;
-  padding: 0.5rem;
-  border-radius: 0.8rem;
-  border: solid 3px #aea0f8;
-  background-color: ${props => props.color};
+  justify-content: space-evenly;
+  border-radius: 0.6rem;
+  border: solid 2px #aea0f8;
+  background-color: #ffffff;
   align-items: center;
   color: #787878;
   font-size: 1.5rem;
   font-weight: bold;
-  margin: 0.5rem;
   cursor: pointer;
   transition: all ease-in-out 0.2s;
   &:hover {
     background-color: #aea0f8;
     color: #ffffff;
     & div {
-      background-color: '';
+      background-color: #ffffff;
     }
   }
 `
 export const Dash = styled.div`
-  width: 0.5rem;
+  width: 0.3rem;
   height: 1.6rem;
   border-radius: 1rem;
   background-color: #aea0f8;
-  margin-right: 0.5rem;
   transition: all ease-in-out 0.2s;
 `
