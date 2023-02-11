e75d1acef2079ab592b10348f001efb539ed6ec3
Marcos Barbosa <mvpbarbosa16@gmail.com>
2023-02-10 21:18:58 -0300

    feat(Chart): implement BarChart folder #5

---
+++ src/components/Chart/BarChart/index.tsx
@@ -0,0 +1,52 @@
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend
+} from 'chart.js'
+
+import { Bar } from 'react-chartjs-2'
+
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend)
+export const BarChart = () => {
+  const formatData = {
+    labels: ['', '', '', '', '', ''],
+    datasets: [
+      {
+        barThickness: 55,
+        label: 'My Data',
+        data: ['5', '10', '15', '20', '25', '25'],
+        backgroundColor: [
+          'red',
+          'yellow ',
+          'orange',
+          'green',
+          'blue',
+          'indigo'
+        ],
+        borderColor: 'black',
+        borderWidth: 1
+      }
+    ]
+  }
+  return (
+    <Bar
+      data={formatData}
+      options={{
+        aspectRatio: 2,
+        responsive: true,
+        layout: { padding: { left: 30, right: 30, top: 40, bottom: 20 } },
+        plugins: {
+          title: {
+            display: true
+          },
+          legend: {
+            display: false
+          }
+        }
+      }}
+    />
+  )
+}
--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,6 +1,5 @@
 // import { v4 as uuidv4 } from 'uuid'
-import { Menu } from './Menu'
-import { useEffect, useState } from 'react'
+import { Menu } from './BarChart/Menu'
 import {
   ChartContainer,
   ChartDiv,
@@ -9,71 +8,15 @@ import {
   Time,
   Title
 } from './styled'
-import {
-  Chart as ChartJS,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-  Tooltip,
-  Legend
-} from 'chart.js'
-
-import { Bar } from 'react-chartjs-2'
-
-ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend)
+import { BarChart } from './BarChart'
 
 export default function Chart(): JSX.Element {
-  // Função para enviar os datos formatados para o gráfico do jeito que ele pede com as labels e os datasets
-  const formatData = {
-    labels: ['', '', '', '', '', ''],
-    datasets: [
-      {
-        barThickness: 55,
-        label: 'My Data',
-        data: ['5', '10', '15', '20', '25', '25'],
-        backgroundColor: [
-          'red',
-          'yellow ',
-          'orange',
-          'green',
-          'blue',
-          'indigo'
-        ],
-        borderColor: 'black',
-        borderWidth: 1
-      }
-    ]
-  }
-
-  const [toggle, setToggle] = useState(true)
-  const [btnColor, setBtnColor] = useState('#ffffff')
-  const [selectedItems, setSelectedItems] = useState<string[]>([])
-
-  useEffect(() => {
-    setBtnColor(state => (toggle ? '#ffffff' : '#aea0f8'))
-  }, [toggle])
-
   return (
     <ChartContainer>
       <Title>ESTATÍSTICAS DA SEMANA</Title>
       <ChartDiv>
         <Menu />
-        <Bar
-          data={formatData}
-          options={{
-            aspectRatio: 2,
-            responsive: true,
-            layout: { padding: { left: 30, right: 30, top: 40, bottom: 20 } },
-            plugins: {
-              title: {
-                display: true
-              },
-              legend: {
-                display: false
-              }
-            }
-          }}
-        />
+        <BarChart />
         <Time>
           <h2>GRÁFICO</h2>
           <SelectTimeButton>
