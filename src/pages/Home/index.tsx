2d9ada4a44725200743aedf2e84d2e4841dd809e
Manoel Rocha <manoelrocha399@gmail.com>
2023-02-11 11:17:00 -0300

    fix(Home): remove calendar #5

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,12 @@
-import Calendar from '../../components/Calendar'
 import Chart from '../../components/Chart'
+import { LineChart } from '../../components/Chart/LineChart'
 import { Container } from './styled'
 
 export default function Home() {
   return (
     <Container>
-      <Calendar />
       <Chart />
+      <LineChart />
     </Container>
   )
 }
