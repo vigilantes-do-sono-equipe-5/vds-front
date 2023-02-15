e50f821c08eb768678cbe0c382ab0e4cc5e26af0
denerbatista <denerbatista@icloud.com>
2023-02-15 16:57:22 -0300

    refactor(contexts): add news providers in `index` #23

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -7,6 +7,10 @@ import { lightTheme } from '../assets/themes/light'
 import Menu from '../components/Menu'
 import { IProvidersProps } from '../interfaces/contexts.interfaces'
 import { ContextCompanyProvider } from './ContextCompany'
+import { ContextGadProvider } from './ContextGad'
+import { ContextIsiProvider } from './ContextIsi'
+import { ContextPhqProvider } from './ContextPhq'
+import { ContextRatingsProvider } from './ContextRatings'
 import { ContextSleepDiariesProvider } from './ContextSleepDiaries'
 
 function Providers({ children }: IProvidersProps): JSX.Element {
@@ -20,11 +24,19 @@ function Providers({ children }: IProvidersProps): JSX.Element {
       <ThemeProvider theme={theme ? lightTheme : DarkTheme}>
         <ContextCompanyProvider>
           <ContextSleepDiariesProvider>
-            <Global>
-              <GlobalStyles />
-              <Menu changeTheme={switchTheme} />
-              {children}
-            </Global>
+            <ContextGadProvider>
+              <ContextIsiProvider>
+                <ContextPhqProvider>
+                  <ContextRatingsProvider>
+                    <Global>
+                      <GlobalStyles />
+                      <Menu changeTheme={switchTheme} />
+                      {children}
+                    </Global>
+                  </ContextRatingsProvider>
+                </ContextPhqProvider>
+              </ContextIsiProvider>
+            </ContextGadProvider>
           </ContextSleepDiariesProvider>
         </ContextCompanyProvider>
       </ThemeProvider>
