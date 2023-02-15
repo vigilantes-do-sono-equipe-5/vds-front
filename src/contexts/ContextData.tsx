d31020a207e640613d6c63c2c3c30610e26add41
denerbatista <denerbatista@icloud.com>
2023-02-15 15:18:17 -0300

    feat(ContextData): add getMainNumbers #23

--- src/contexts/ContextData.tsx
+++ src/contexts/ContextData.tsx
@@ -1,13 +1,18 @@
-import { AxiosError, AxiosResponse } from 'axios'
+import { AxiosResponse } from 'axios'
 import { createContext, useContext, useEffect, useState } from 'react'
 import { api } from '../helpers/api'
-import { ICompany, IDataContext } from '../interfaces/contextData.interfaces'
+import {
+  ICompany,
+  IDataContext,
+  IMainNumbers
+} from '../interfaces/contextData.interfaces'
 import { IAllProvidersProps } from '../interfaces/contexts.interfaces'
 
 const DataContext = createContext<IDataContext | {}>({})
 
 export function ContextDataProvider({ children }: IAllProvidersProps) {
   const [companies, setCompanies] = useState<ICompany[]>()
+  const [mainNumbers, setMainNumbers] = useState<IMainNumbers>()
 
   const getCompanies = async (): Promise<void> => {
     try {
@@ -18,8 +23,22 @@ export function ContextDataProvider({ children }: IAllProvidersProps) {
     }
   }
 
+  const getMainNumbers = async (id: string): Promise<void> => {
+    try {
+      const response: AxiosResponse<IMainNumbers> = await api.get(
+        `/company/main-numbers/${id}`
+      )
+      setMainNumbers(response.data)
+    } catch (error) {
+      console.log('mainNumbers', error)
+    }
+  }
+
   return (
-    <DataContext.Provider value={{ company: { companies, getCompanies } }}>
+    <DataContext.Provider
+      value={{
+        company: { companies, getCompanies, mainNumbers, getMainNumbers }
+      }}>
       {children}
     </DataContext.Provider>
   )
--- src/interfaces/contextData.interfaces.ts
+++ src/interfaces/contextData.interfaces.ts
@@ -5,6 +5,17 @@ export interface ICompany {
   activeEmployees: number
 }
 
+export interface IMainNumbers {
+  userProgramSession: number
+  sleepDiaries: number
+  techniques: number
+}
+
 export interface IDataContext {
-  company: { companies: ICompany[]; getCompanies: () => Promise<void> }
+  company: {
+    companies: ICompany[]
+    getCompanies: () => Promise<void>
+    mainNumbers: IMainNumbers
+    getMainNumbers: () => Promise<void>
+  }
 }
